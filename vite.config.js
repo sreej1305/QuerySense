e0c7c683644337a2454729eaef9f4dba64aa5f54
sreej1305 <sreejapariveda1305@gmail.com>
2026-01-02 21:26:50 +0530

    Set base path to /QuerySense/ for working subdomain link

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ const __dirname = path.dirname(__filename)
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/QuerySense/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
