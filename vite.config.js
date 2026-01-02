5868b0e5e03b1a6209ec15244d5b761eda1d6f56
sreej1305 <sreejapariveda1305@gmail.com>
2026-01-02 21:01:39 +0530

    Configure custom domain www.querysense.ai

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ const __dirname = path.dirname(__filename)
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/QuerySense/',
+  base: '/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
